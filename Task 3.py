309279f8b3105f49b489981965b1c48fe6e3e154
Prokash Sikder <prokash811@gmail.com>
2020-07-10 12:38:38 +0600

    Update Task 3.py

--- Task 3.py
+++ Task 3.py
@@ -1,11 +1,8 @@
-def strCheck(string_1,string_2):
-    count =0
-    for i in range(len(string_1)):
-        if string_1[i]==string_2[0]:
-            if string_1[i:i+len(string_2)] == string_2:
-               count+=1;
-    return count
-string_1 = input("string_1 = ")
-string_2 = input("string_2 = ")
-a = strCheck(string_1,string_2)
+def muri():
+    string_1 = input("string_1 = ")
+    string_2 = input("string_2 = ")
+
+    return string_1.count(string_2)
+
+a = muri()
 print("Here string_2 occur in string_1", a, "times")
